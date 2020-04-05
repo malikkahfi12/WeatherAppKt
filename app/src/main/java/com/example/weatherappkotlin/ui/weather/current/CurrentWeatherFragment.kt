922a4f154c048218cdc87d9dcc4827c160c865af
malikkahfi12 <malikkahfi1996@gmail.com>
2020-04-05 21:12:55 +0700

    Retrofit data

--- app/src/main/java/com/example/weatherappkotlin/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/example/weatherappkotlin/ui/weather/current/CurrentWeatherFragment.kt
@@ -8,6 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.weatherappkotlin.R
+import com.example.weatherappkotlin.data.ApixuWeatherApiService
+import kotlinx.android.synthetic.main.current_weather_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CurrentWeatherFragment : Fragment() {
 
@@ -29,6 +34,11 @@ class CurrentWeatherFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(CurrentWeatherViewModel::class.java)
         // TODO: Use the ViewModel
+        val apiService = ApixuWeatherApiService()
+        GlobalScope.launch(Dispatchers.Main) {
+            val currentWeatherResponse = apiService.getCurrentWeather("London").await()
+            textView.text = currentWeatherResponse.toString()
+        }
     }
 
 }
